dece9ba827c8d9c69f29f95488ad4eb2c81d919f
Omar <Omar>
2025-11-21 03:34:06 +0200

    fixed the button group

--- src/components/Omar_components/SearchBar.tsx
+++ src/components/Omar_components/SearchBar.tsx
@@ -1,25 +1,33 @@
-import { SearchIcon } from "lucide-react"
-
-import { Button } from "@/components/ui/button"
-import { ButtonGroup } from "@/components/ui/button-group"
-import { Input } from "@/components/ui/input"
-
-type ButtonGroupProps<T> =
-{
-  classnameGroup? :string ;
-  classnameInput? :string  ;
-  onClick?: ()=>void ;
-  onChange? : (value :T)=>void;
-}
+import { SearchIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { ButtonGroup } from "@/components/ui/button-group";
+import { Input } from "@/components/ui/input";
 
 
-export function ButtonGroupInput( {classnameGroup , classnameInput ,onClick , onChange}:ButtonGroupProps) {
+type ButtonGroupProps<T> = {
+  classnameGroup?: string;
+  classnameInput?: string;
+  onClick?: () => void;
+  onChange?: (value: T) => void;
+};
+
+
+export function ButtonGroupInput<T>({
+  classnameGroup,
+  classnameInput,
+  onClick,
+  onChange,
+}: ButtonGroupProps<T>) {
   return (
     <ButtonGroup className={`${classnameGroup}`}>
-      <Input placeholder="Search..." className={`${classnameInput}`} onChange={onChange} />
+      <Input
+        placeholder="Search..."
+        className={`${classnameInput}`}
+        onChange={(e) => onChange && onChange(e.target.value as unknown as T)}
+      />
       <Button variant="outline" aria-label="Search" onClick={onClick}>
         <SearchIcon />
       </Button>
     </ButtonGroup>
-  )
+  );
 }
