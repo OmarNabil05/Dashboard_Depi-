224b664245065c9e401876e31b0bc6c2d92e8530
Vivian <vivianboshra91@gmail.com>
2025-11-29 14:23:23 +0200

    added the menu icon in the navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
-import MainLayout from './pages/mainlayout'
+import MainLayout from './pages/MainLayout'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import StatisticsPage from './pages/StatisticsPage';
 import Log from './pages/Log';
 import Tables from './pages/Tables';
 import Requests from './pages/Requests';
+import { Menu } from 'lucide-react';
+import MenuPage from './pages/Menu';
 
 
 const router = createBrowserRouter([
@@ -16,6 +18,10 @@ const router = createBrowserRouter([
         {
           path: '/Stat'
           , element: <StatisticsPage />
+        },
+        {
+          path: '/menu'
+          , element: <MenuPage />
         },
         {
           path: '/Log'
--- src/pages/MainLayout.tsx
+++ src/pages/MainLayout.tsx
@@ -1,13 +1,15 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import { Outlet } from "react-router-dom";
 import Navbar, { type NavItem } from "@/components/ui/navbar";
-import { BarChart3, Scroll, Coffee, Package } from "lucide-react";
+import { BarChart3, Scroll, Coffee, Package ,SquareMenu } from "lucide-react";
 
 const navItems: NavItem[] = [
   { path: "/Stat", icon: <BarChart3 size={24} color="#f97316" /> },   // orange
-  { path: "/Log", icon: <Scroll size={24} color="#10b981" /> },       // green
+  { path: "/Log", icon: <Scroll size={24} color="#10b981" /> },   
+  { path: "/Menu", icon: <SquareMenu size={24} color="#FFFF00" /> }, // yellow
   { path: "/Tables", icon: <Coffee size={24} color="#3b82f6" /> },    // blue
   { path: "/Requests", icon: <Package size={24} color="#e11d48" /> }, // red
+ 
 ];
 
 export default function MainLayout() {
